69e6b6d6b40e1c907ae503fdc507e204114a22f1
narainkarthikv <narainkarthik812@gmail.com>
2024-03-08 09:53:28 +0530

    updated hooks

--- frontend/src/components/CreateUser.component.jsx
+++ frontend/src/components/CreateUser.component.jsx
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './css/CreateUser.component.css';
 
 const CreateUser = () => {
   const [username, setUsername] = useState('');
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://fit-track-epab.onrender.com/users')
+      .then(response => {
+        if (response.data.length > 0) {
+          setUsers("");
+          setUsername("");
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching users:', error);
+      });
+  }, []);
 
   const onChangeUsername = (e) => {
     setUsername(e.target.value);
@@ -12,30 +26,29 @@ const CreateUser = () => {
   const onSubmit = (e) => {
     e.preventDefault();
 
+    console.log(users);
     const user = {
       username: username
     };
 
-    axios.post('https://fit-track-epab.onrender.com/add', user)
+    axios.post('https://fit-track-epab.onrender.com/users/add', user)
       .then(res => {
         console.log(res.data);
         setUsername('');
-         
+        window.location = '/users'; // Redirect to the users page
       })
       .catch(error => {
         console.error('Error creating user:', error);
       });
-
-      window.location = '/users';
   };
 
   return (
     <div>
       <form onSubmit={onSubmit} className='users-form-container'>
         <div className='users-form-group'>
-          <label htmlFor="username">Username:</label>
+          <label>Username:</label>
           <input
-          id="username"
+            id="username"
             type="text"
             required
             className="users-form-control"
